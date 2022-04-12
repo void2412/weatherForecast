e61a9bd92fb697b638068d83da6ef2421b38bba5
Chi Hieu Nguyen <void2412@gmail.com>
2022-04-12 16:55:06 +0930

    finished api call

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var searchHistoryArea = $('#searchHistoryArea')
 var infoArea = $('#infoArea')
 var searchHistoryList = JSON.parse(localStorage.getItem('searchHistory'))||[]
 const maxSearchHistorySize = 10
-
+var apiKey = "cc09dda6a0046068b344f5f5d0903567"
+var test
 function init(){
     displaySearchHistory()
 }
@@ -12,12 +13,14 @@ init()
 
 function searchBtnClicked(event){
     event.preventDefault()
-    var cityName = cityNameInput.val()
+    var cityName = cityNameInput.val().trim()
+    searchForCity(cityName)
     updateSearchHistory(cityName)
     clearSearchHistory()
     displaySearchHistory()
 }
 
+// search History related
 function handleSearchHistory(event) {
 
 }
@@ -47,11 +50,38 @@ function clearSearchHistory(){
     searchHistoryArea.children().remove()
 }
 
-function searchForCity(cityName){
+
+// api call related
+async function searchForCity(cityName){
+    var coord = await getCityCoord(cityName)
+    var data = await fetch(getOneCallURL(coord)).json()
+    displayCurrentData(data)
+    display5DayData(data)
+}
+
+async function getCityCoord(cityName){
+    var response = await fetch(getGeocodeURL(cityName))
+    var data = await response.json()
+    var coord = {lat:data[0].lat,lon:data[0].lon}
+    return coord
+}
+
+function getGeocodeURL(cityName){
+    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey + "&limit=1"
+    return url
+}
+
+function getOneCallURL(coord){
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&appid=" + apiKey + "&units=metric"
+    return url
+}
+
+// display api data
+function displayCurrentData(data){
 
 }
 
-function displayCityData(data){
+function display5DayData(data){
 
 }
 
@@ -60,4 +90,4 @@ function clearInfo(){
 }
 
 searchBtn.on('click', searchBtnClicked)
-searchHistoryArea.on('click', '.historyBtn', searchHistoryAreaClicked)
+searchHistoryArea.on('click', '.historyBtn', handleSearchHistory)
