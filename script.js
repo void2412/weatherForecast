ec047f8d6fcdfba1857ff65b9abdcbe7a550070a
Chi Hieu Nguyen <void2412@gmail.com>
2022-04-12 15:10:35 +0930

    search history finished

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ var cityNameInput = $('#cityNameInput')
 var searchHistoryArea = $('#searchHistoryArea')
 var infoArea = $('#infoArea')
 var searchHistoryList = JSON.parse(localStorage.getItem('searchHistory'))||[]
+const maxSearchHistorySize = 10
 
-function searchBtnClicked(){
-    
+function init(){
+    displaySearchHistory()
+}
+init()
+
+function searchBtnClicked(event){
+    event.preventDefault()
+    var cityName = cityNameInput.val()
+    updateSearchHistory(cityName)
+    clearSearchHistory()
+    displaySearchHistory()
 }
 
 function handleSearchHistory(event) {
@@ -13,11 +23,24 @@ function handleSearchHistory(event) {
 }
 
 function updateSearchHistory(cityName){
-
+    if (cityName == ''){
+        return
+    }
+    if (!searchHistoryList.includes(cityName)){
+        searchHistoryList.splice(0, 0, cityName)
+        while (searchHistoryList.length > maxSearchHistorySize){
+            searchHistoryList.pop()
+        }
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistoryList))
+    }
 }
 
 function displaySearchHistory(){
-
+    searchHistoryList = JSON.parse(localStorage.getItem('searchHistory'))||[]
+    for (let index = 0; index < searchHistoryList.length; index++) {
+        var btnElement = $('<button>').addClass('btn btn-secondary form-control mb-3').text(searchHistoryList[index])
+        searchHistoryArea.append(btnElement)
+    }
 }
 
 function clearSearchHistory(){
